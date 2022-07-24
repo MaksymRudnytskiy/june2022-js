292f5aa728345f1702abf6cd90f1dc255b8d230a
MaksymRudnytskiy <therudmax@gmail.com>
2022-07-24 12:21:40 +0300

    lesson 1

---
+++ lesson1/main.js
@@ -0,0 +1,123 @@
+// - Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+
+
+let hello = 'hello'
+
+let owu = 'owu'
+
+let com = 'com'
+
+let ua = 'ua'
+
+let num1 = 1
+
+let num10 = 10
+
+let num999 = -999
+
+let num123 = 123
+
+let num314 = 3.14
+
+let num27 = 2.7
+
+let num16 = 16
+
+let booleantrue = true
+
+let booleanfalse = false
+
+
+console.log(hello)
+alert(hello)
+document.write(hello)
+
+console.log(owu)
+alert(owu)
+document.write(owu)
+
+console.log(com)
+alert(com)
+document.write(com)
+
+console.log(ua)
+alert(ua)
+document.write(ua)
+
+console.log(num1)
+alert(num1)
+document.write(num1)
+
+console.log(num10)
+alert(num10)
+document.write(num10)
+
+console.log(num999)
+alert(num999)
+document.write(num999)
+
+console.log(num123)
+alert(num123)
+document.write(num123)
+
+console.log(num314)
+alert(num314)
+document.write(num314)
+
+console.log(num27)
+alert(num27)
+document.write(num27)
+
+console.log(num16)
+alert(num16)
+document.write(num16)
+
+console.log(booleantrue)
+alert(booleantrue)
+document.write(booleantrue)
+
+console.log(booleanfalse)
+alert(booleanfalse)
+document.write(booleanfalse)
+
+
+// - Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
+let firstName = 'Максим'
+
+let middleName = 'Богданович'
+
+let lastName = 'Рудницький'
+
+let fullName = `${lastName} ${firstName} ${middleName}`
+
+
+console.log(fullName)
+alert(fullName)
+document.write(fullName)
+
+
+// - За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+//     let a = 100; let b = '100'; let c = true;
+
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+
+// - За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
+let name = prompt('Імя')
+let middleName2 = prompt('По-Батькові')
+let age = prompt('Вік')
+
+console.log(name)
+console.log(middleName2)
+console.log(age)
+
