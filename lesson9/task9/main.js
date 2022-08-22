027dc5a5a5dc0cb3c5f275b91290c93bb0311daf
MaksymRudnytskiy <therudmax@gmail.com>
2022-08-22 12:54:26 +0300

    lesson 9 part 8

--- lesson9/task9/main.js
+++ lesson9/task9/main.js
@@ -1,3 +1,22 @@
 // *** Створити 3 инпута та кнопку. Один визначає кількість рядків, другий - кількість ячеєк, третій вмиіст ячеєк.
 //     При натисканні кнопки, вся ця інформація зчитується і формується табличка, з відповідним вмістом.
 // (Додатковачастина для завдання)
+
+
+
+
+
+
+let btn = document.getElementsByTagName('button')[0]
+
+let line = document.getElementsByClassName('input1')
+let cell = document.getElementsByClassName('input2')
+let content = document.getElementsByClassName('input3')
+
+
+
+btn.onclick = function (e) {
+    e.preventDefault()
+
+
+};
