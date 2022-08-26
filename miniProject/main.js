9171adaac08da9474c21c5db8d6cd9721b4ed8c5
MaksymRudnytskiy <therudmax@gmail.com>
2022-08-26 23:17:20 +0300

    lesson 10 +

--- miniProject/main.js
+++ miniProject/main.js
@@ -1,35 +1,73 @@
-fetch('https://jsonplaceholder.typicode.com/users')
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(response => response.json())
+//     .then(users => users.forEach(user => {
+//             let div = document.createElement('div')
+//             div.classList.add('item')
+//
+//             let userID = document.createElement('div')
+//             userID.classList.add('id')
+//             userID.innerHTML = `ID: ${user.id}`
+//             div.appendChild(userID)
+//
+//             let userName = document.createElement('div')
+//             userName.classList.add('username')
+//             userName.innerHTML = `Username: ${user.username}`
+//             div.appendChild(userName)
+//
+//             let a = document.createElement('a')
+//             a.classList.add('button')
+//             a.innerText = 'Details'
+//             a.href = `details/details.html?id=${user.id}`
+//             div.appendChild(a)
+//
+//             // let button = document.createElement('button')
+//             // button.classList.add('button')
+//             // button.innerText = 'Details'
+//             // div.appendChild(button)
+//             // button.onclick = () => {
+//             //     location.href = `details/details.html?user=${user.id}`
+//             // }
+//
+//             document.body.appendChild(div)
+//
+//
+//         }
+//     ))
+
+fetch(`https://jsonplaceholder.typicode.com/users`)
     .then(response => response.json())
-    .then(users => users.forEach(user => {
-            let div = document.createElement('div')
-            div.classList.add('item')
+    .then(users => {
 
-            let userID = document.createElement('div')
-            userID.classList.add('id')
-            userID.innerHTML = `ID: ${user.id}`
-            div.appendChild(userID)
+            for (const user of users) {
+                let div = document.createElement('div')
+                div.classList.add('item')
 
-            let userName = document.createElement('div')
-            userName.classList.add('username')
-            userName.innerHTML = `Username: ${user.username}`
-            div.appendChild(userName)
+                let userID = document.createElement('div')
+                userID.classList.add('id')
+                userID.innerHTML = `ID: ${user.id}`
+                div.appendChild(userID)
 
-            let a = document.createElement('a')
-            a.classList.add('button')
-            a.innerText = 'Details'
-            a.href = `details/details.html?id=${user.id}`
-            div.appendChild(a)
+                let userName = document.createElement('div')
+                userName.classList.add('username')
+                userName.innerHTML = `Username: ${user.username}`
+                div.appendChild(userName)
 
-            // let button = document.createElement('button')
-            // button.classList.add('button')
-            // button.innerText = 'Details'
-            // div.appendChild(button)
-            // button.onclick = () => {
-            //     location.href = `details/details.html?user=${user.id}`
-            // }
+                let a = document.createElement('a')
+                a.classList.add('button')
+                a.innerText = 'Details'
+                a.href = `details/details.html?id=${user.id}`
+                div.appendChild(a)
 
-            document.body.appendChild(div)
+                // let button = document.createElement('button')
+                // button.classList.add('button')
+                // button.innerText = 'Details'
+                // div.appendChild(button)
+                // button.onclick = () => {
+                //     location.href = `details/details.html?user=${user.id}`
+                // }
 
-
+                document.body.appendChild(div)
+            }
+//
         }
-    ))
+    )
